eeb1be47c8cab37cba1dd6bfeabba47a9a20a665
Khoa Pham <j.dangkhoa.118@gmail.com>
2022-04-26 01:35:00 +0700

    edit db script

--- code/db_script/create_account.sql
+++ code/db_script/create_account.sql
@@ -13,53 +13,85 @@ use [Profile]
 
 go
 
-Create table [ACCOUNT] (
-	[id] Uniqueidentifier NOT NULL default(newid()),
-	[username] varchar(255) NOT NULL,
-	[password] varchar(255) NOT NULL,
-	[type] varchar(255) NOT NULL check (type in ('ADMIN', 'CUSTOMER', 'PARTNER')),
-Primary Key  ([id])
+-- Create table [ACCOUNT] (
+-- 	[id] Uniqueidentifier NOT NULL default(newid()),
+-- 	[username] varchar(255) NOT NULL,
+-- 	[password] varchar(255) NOT NULL,
+-- 	[type] varchar(255) NOT NULL check (type in ('ADMIN', 'CUSTOMER', 'PARTNER')),
+-- Primary Key  ([id])
+-- ) 
+-- go
+
+-- Create table [CUSTOMER] (
+-- 	[account_id] Uniqueidentifier NOT NULL default(newid()),
+-- 	[name] Nvarchar(255) NOT NULL,
+-- 	[gender] Bit NOT NULL,
+-- 	[birthday] Datetime NOT NULL,
+-- 	[address] Nvarchar(255) NOT NULL,
+-- 	[email] varchar(255) NOT NULL,
+-- 	[phone] Numeric(11,0) NOT NULL,
+-- Primary Key  ([account_id])
+-- ) 
+-- go
+
+-- Create table [PARTNER] (
+-- 	[account_id] Uniqueidentifier NOT NULL default(newid()),
+-- 	[name] Nvarchar(255) NOT NULL,
+-- 	[phone] Numeric(11,0) NOT NULL,
+-- 	[email] varchar(255) NOT NULL,
+-- 	[job] Nvarchar(255) NOT NULL,
+-- 	[company_name] Nvarchar(255) NOT NULL,
+-- 	[country] Nvarchar(255) NOT NULL,
+-- 	[office_address] Nvarchar(255) NOT NULL,
+-- 	[office_phone] Numeric(11,0) NOT NULL,
+-- Primary Key  ([account_id])
+-- ) 
+-- go
+
+Create table [COMPANY] (
+	[company_id] Uniqueidentifier NOT NULL,
+	[name] Nvarchar(255) NULL,
+	[location] Nvarchar(255) NULL,
+	[phone] Numeric(11,0) NULL,
+	[country] Nvarchar(255) NULL,
+Primary Key  ([company_id])
 ) 
 go
 
-Create table [CUSTOMER] (
-	[account_id] Uniqueidentifier NOT NULL default(newid()),
-	[name] Nvarchar(255) NOT NULL,
-	[gender] Bit NOT NULL,
-	[birthday] Datetime NOT NULL,
-	[address] Nvarchar(255) NOT NULL,
-	[email] varchar(255) NOT NULL,
-	[phone] Numeric(11,0) NOT NULL,
-Primary Key  ([account_id])
+Create table [USER] (
+	[user_id] Uniqueidentifier NOT NULL,
+	[username] Varchar(255) NULL,
+	[password] Varchar(255) NULL,
+	[name] Nvarchar(255) NULL,
+	[email] Nvarchar(255) NULL,
+	[gender] Bit NULL,
+	[dob] Datetime NULL,
+	[address] Nvarchar(255) NULL,
+	[job] Nvarchar(255) NULL,
+	[icompany_id] Uniqueidentifier NOT NULL,
+Primary Key  ([user_id])
 ) 
 go
 
-Create table [PARTNER] (
-	[account_id] Uniqueidentifier NOT NULL default(newid()),
-	[name] Nvarchar(255) NOT NULL,
-	[phone] Numeric(11,0) NOT NULL,
-	[email] varchar(255) NOT NULL,
-	[job] Nvarchar(255) NOT NULL,
-	[company_name] Nvarchar(255) NOT NULL,
-	[country] Nvarchar(255) NOT NULL,
-	[office_address] Nvarchar(255) NOT NULL,
-	[office_phone] Numeric(11,0) NOT NULL,
-Primary Key  ([account_id])
+Create table [USER_TYPE] (
+	[type_id] Uniqueidentifier NOT NULL,
+	[name] Nvarchar(255) NULL,
+Primary Key  ([type_id])
 ) 
 go
 
 Create table [REWARD] (
-	[customer_id] Uniqueidentifier NOT NULL default(newid()),
+	[user_id] Uniqueidentifier NOT NULL default(newid()),
 	[reward] Numeric(18,0) NOT NULL,
 	[value] Numeric(18,0) NOT NULL,
-Primary Key  ([customer_id])
+Primary Key  ([user_id])
 ) 
 go
 
 Create table [SERVICE] (
-	[partner_id] Uniqueidentifier NOT NULL default(newid()),
+	[company_id] Uniqueidentifier NOT NULL default(newid()),
 	[service_name] Nvarchar(255) NOT NULL,
-Primary Key  ([partner_id])
+Primary Key  ([company_id])
 ) 
 go
 
@@ -80,23 +112,23 @@ go
 
 
 insert into SERVICE
-values (newid(), N'Chuyến bay')
+values (newid(), 'FLIGHT')
 insert into SERVICE
-values (newid(), N'Khách sạn')
+values (newid(), 'HOTEL')
 insert into SERVICE
-values (newid(), N'Ðưa dón sân bay')
+values (newid(), 'AIRPORT PICKLES')
 insert into SERVICE
-values (newid(), N'Biệt thự và can hộ')
+values (newid(), 'VILLA APARTMENT')
 insert into SERVICE
-values (newid(), N'Tour du lịch')
+values (newid(), 'TOUR')
 insert into SERVICE
-values (newid(), N'Thuê xe')
+values (newid(), 'CAR RENTAL')
 insert into SERVICE
-values (newid(), N'Nhà hàng')
+values (newid(), 'RESTAURANT')
 insert into SERVICE
-values (newid(), N'Voucher')
+values (newid(), 'VOUCHER')
 insert into SERVICE
-values (newid(), N'Combo tiết kiệm')
+values (newid(), 'SAVING COMBO')
 
 
 insert into ACCOUNT
