97924fe407c29de1f9efe33be7e748468cf8d4cb
Pham Dang Khoa <19dh111073@st.huflit.edu.vn>
2022-05-11 13:44:38 +0700

    user register component

--- code/client/src/router.tsx
+++ code/client/src/router.tsx
@@ -8,11 +8,15 @@ import {
 	Home,
 	Order,
 } from "./pages";
+import { UserHome } from "./pages/User/UserHome";
+import { UserProfile } from "./pages/User/UserProfile";
+import { UserRegister } from "./pages/User/UserRegister";
 
 // u can add new route in here
 export const routerConfig = [
 	{ path: "/", component: Home, exact: true },
 	{ path: "/login", component: Login },
+
 	{ path: "/vouchers", component: Voucher, isPrivate: true },
 	{ path: "/orders", component: Order, isPrivate: true },
 	{
@@ -26,6 +30,9 @@ export const routerConfig = [
 		component: Profile,
 		isPrivate: true,
 	},
+	{ path: "/home", component: UserHome },
+	{ path: "/user-profile", component: UserProfile },
+	{ path: "/register/customer", component: UserRegister },
 	{ path: "*", component: NotFound, exact: true },
 ];
 
