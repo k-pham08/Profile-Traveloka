2b7acd7099ab5ff231e5ded4334dc0ab929825ed
vinhphan812 <vinhphan812@gmail.com>
2022-05-21 02:31:24 +0700

    feat: :fire: Login with callback for other services
    - Login with callback for other services call it. task #53

--- code/client/src/pages/Login.tsx
+++ code/client/src/pages/Login.tsx
@@ -1,132 +1,156 @@
-import {Avatar, Box, Button, Grid, TextField, Typography} from "@mui/material";
-import {FC, useEffect, useState} from "react";
-import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import {
+	Avatar,
+	Box,
+	Button,
+	Grid,
+	TextField,
+	Typography,
+} from "@mui/material";
+import { FC, useEffect, useState } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
-import {APP_NAME, PRIMARY_COLOR, srcIcon} from "../utils/constraint";
-import {setTitle} from "../utils/set_title";
-import {useStore} from "../stores";
-import {useSnackbar} from "notistack";
-import {ERROR} from "../utils/messageTerms";
-import {observer} from "mobx-react";
-import {Link} from "react-router-dom";
-import {Divider} from "@mui/material/";
-import {theme} from "../utils/theme";
+import { APP_NAME, PRIMARY_COLOR, srcIcon } from "../utils/constraint";
+import { setTitle } from "../utils/set_title";
+import { useStore } from "../stores";
+import { useSnackbar } from "notistack";
+import { ERROR } from "../utils/messageTerms";
+import { observer } from "mobx-react";
+import { Link, useSearchParams } from "react-router-dom";
+import { theme } from "../utils/theme";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            backgroundColor: PRIMARY_COLOR,
-        },
-        loginForm: {
-            backgroundColor: "#fff",
-            padding: "3rem 5rem 1rem 5rem !important",
-        },
-        marginTop: {
-            marginTop: "1rem !important",
-        },
-        avatar: {
-            marginRight: "auto",
-            marginLeft: "auto",
-            marginBottom: "1rem",
-        },
-    })
+	createStyles({
+		root: {
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "center",
+			alignItems: "center",
+			height: "100vh",
+			backgroundColor: PRIMARY_COLOR,
+		},
+		loginForm: {
+			backgroundColor: "#fff",
+			padding: "3rem 5rem 1rem 5rem !important",
+		},
+		marginTop: {
+			marginTop: "1rem !important",
+		},
+		avatar: {
+			marginRight: "auto",
+			marginLeft: "auto",
+			marginBottom: "1rem",
+		},
+	})
 );
 
 const Copyright: FC = () => {
-    return (
-        <Typography variant="body2" align="center">
-            Copyright © Traveloka {new Date().getFullYear()}.
-        </Typography>
-    );
+	return (
+		<Typography variant="body2" align="center">
+			Copyright © Traveloka {new Date().getFullYear()}.
+		</Typography>
+	);
 };
 
 const NotHaveAccount: FC = () => {
-    return <>
-        {/*<Divider orientation="horizontal">*/}
-        {/*    <Typography margin={theme.spacing(1)}>Hoặc</Typography>*/}
-        {/*</Divider>*/}
-
-        <Typography>Bạn chưa có tài khoản? <Link to={"/register"} style={{color: PRIMARY_COLOR, marginTop: theme.spacing(2)}}>Đăng kí</Link></Typography>
-    </>
-}
+	return (
+		<>
+			<Typography>
+				Bạn chưa có tài khoản?{" "}
+				<Link
+					to={"/signup"}
+					style={{
+						color: PRIMARY_COLOR,
+						marginTop: theme.spacing(2),
+					}}
+				>
+					Đăng kí
+				</Link>
+			</Typography>
+		</>
+	);
+};
 
 export const Login: FC<{}> = observer(() => {
-    const {sSignIn} = useStore();
-    const {enqueueSnackbar} = useSnackbar();
-    const classes = useStyles();
-    const [submitting, setSubmitting] = useState(false);
-    useEffect(() => {
-        setTitle("Đăng Nhập Traveloka");
-    }, []);
+	const { sSignIn, token } = useStore();
+	const { enqueueSnackbar } = useSnackbar();
+	const classes = useStyles();
+	const [submitting, setSubmitting] = useState(false);
+	const callback: string = useSearchParams()[0].get("callback") || "";
+	useEffect(() => {
+		setTitle("Đăng Nhập Traveloka");
+	}, []);
 
-    const handleLogin = () => {
-        if (!sSignIn.username || !sSignIn.password) {
-            enqueueSnackbar(ERROR.LOGIN_INVALID, {variant: "error"});
-            return;
-        }
+	const handleLogin = () => {
+		if (!sSignIn.username || !sSignIn.password) {
+			enqueueSnackbar(ERROR.LOGIN_INVALID, { variant: "error" });
+			return;
+		}
 
-        setSubmitting(true);
-        sSignIn.doLogin().then((err) => {
-            if (err) return enqueueSnackbar(err, {variant: "error"});
-        }).catch(function (error) {
-            enqueueSnackbar(error.message, {variant: "error"})
-        }).finally(() => setSubmitting(false));
-    };
-    return (
-        <div className={classes.root}>
-            <Grid container direction="column" alignItems="center">
-                <Grid item xs={1} sm={3} className={classes.loginForm}>
-                    <Avatar
-                        className={classes.avatar}
-                        variant="square"
-                        src={srcIcon}
-                        sx={{width: 64, height: 64}}
-                    />
-                    <Typography align="center" variant="h4">
-                        Login {APP_NAME}
-                    </Typography>
+		setSubmitting(true);
+		sSignIn
+			.doLogin(callback)
+			.then((err) => {
+				if (err) return enqueueSnackbar(err, { variant: "error" });
+			})
+			.catch(function (error) {
+				enqueueSnackbar(error.message, { variant: "error" });
+			})
+			.finally(() => setSubmitting(false));
+	};
+	return (
+		<div className={classes.root}>
+			<Grid container direction="column" alignItems="center">
+				<Grid item xs={1} sm={3} className={classes.loginForm}>
+					<Avatar
+						className={classes.avatar}
+						variant="square"
+						src={srcIcon}
+						sx={{ width: 64, height: 64 }}
+					/>
+					<Typography align="center" variant="h4">
+						Login {APP_NAME}
+					</Typography>
 
-                    <TextField
-                        fullWidth={true}
-                        id="username"
-                        label="Tên Đăng Nhập"
-                        variant="outlined"
-                        className={classes.marginTop}
-                        defaultValue={sSignIn.username}
-                        onChange={(event) => sSignIn.set_username(event.target.value)}
-                    />
-                    <TextField
-                        fullWidth={true}
-                        id="password"
-                        label="Mật Khẩu"
-                        type="password"
-                        variant="outlined"
-                        className={classes.marginTop}
-                        defaultValue={sSignIn.password}
-                        onChange={(event) => sSignIn.set_password(event.target.value)}
-                    />
-                    <Button
-                        fullWidth={true}
-                        variant="contained"
-                        className={classes.marginTop}
-                        onClick={handleLogin}
-                        disabled={submitting}
-                    >
-                        Đăng nhập
-                    </Button>
+					<TextField
+						fullWidth={true}
+						id="username"
+						label="Tên Đăng Nhập"
+						variant="outlined"
+						className={classes.marginTop}
+						defaultValue={sSignIn.username}
+						onChange={(event) =>
+							sSignIn.set_username(event.target.value)
+						}
+					/>
+					<TextField
+						fullWidth={true}
+						id="password"
+						label="Mật Khẩu"
+						type="password"
+						variant="outlined"
+						className={classes.marginTop}
+						defaultValue={sSignIn.password}
+						onChange={(event) =>
+							sSignIn.set_password(event.target.value)
+						}
+					/>
+					<Button
+						fullWidth={true}
+						variant="contained"
+						className={classes.marginTop}
+						onClick={handleLogin}
+						disabled={submitting}
+					>
+						Đăng nhập
+					</Button>
 
-                    <NotHaveAccount/>
+					<NotHaveAccount />
 
-                    <Box mt={6}>
-                        <Copyright/>
-                    </Box>
-                </Grid>
-            </Grid>
-        </div>
-    );
+					<Box mt={6}>
+						<Copyright />
+					</Box>
+				</Grid>
+			</Grid>
+		</div>
+	);
 });
