74b5bc5aa57d135352c8bdb423a765d5e96f2fc0
Pham Dang Khoa <19dh111073@st.huflit.edu.vn>
2022-05-27 12:57:05 +0700

    temp

--- code/server/src/service-classify/dto/create-service-classify.dto.ts
+++ code/server/src/service-classify/dto/create-service-classify.dto.ts
@@ -4,5 +4,9 @@ export class CreateServiceClassifyDto {
      @ApiProperty()
      classifyCode: string;
      @ApiProperty()
+     max_price: string;
+     @ApiProperty()
+     min_price: string;
+     @ApiProperty()
      serviceCode: string;
 }
--- code/server/src/service-classify/dto/update-service-classify.dto.ts
+++ code/server/src/service-classify/dto/update-service-classify.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateServiceClassifyDto } from './create-service-classify.dto';
+import { PartialType } from "@nestjs/swagger";
+import { CreateServiceClassifyDto } from "./create-service-classify.dto";
 
 export class UpdateServiceClassifyDto extends PartialType(CreateServiceClassifyDto) {}
