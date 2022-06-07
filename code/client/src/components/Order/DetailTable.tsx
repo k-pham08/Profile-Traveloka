2d46ac38df797b3091a9078eb5b662ef3ad25489
Pham Dang Khoa <19dh111073@st.huflit.edu.vn>
2022-06-08 02:11:28 +0700

    update CRUD Order

--- code/client/src/components/Order/DetailTable.tsx
+++ code/client/src/components/Order/DetailTable.tsx
@@ -8,6 +8,7 @@ import {useNavigate} from "react-router-dom";
 import {OrderDetail} from "../../models/OrderDetail";
 import {useParams} from "react-router";
 import {Order} from "../../models/Order";
+import { rejects } from "assert";
 
 export const DetailTable: FC = observer(() => {
     const {enqueueSnackbar} = useSnackbar();
@@ -24,26 +25,44 @@ export const DetailTable: FC = observer(() => {
                 sOrderDetail.set_order(data);
             })
         }
-    }, [enqueueSnackbar, id, sOrderDetail]); 
+    }, [id]); 
 
     const columns: Column<OrderDetail>[] = [
         {title: "Product name", field: "productName"},
-        {title: "Quantity", field: "quantity"},
-        {title: "Price", field: "price"},
+        {title: "Quantity", field: "quantity", type: "numeric"},
+        {title: "Price", field: "price", type: "numeric"},
         {title: "Thumbnail", field: "thumbnail"},
     ];
 
     return <MaterialTable 
     style={generalStyles.paddingTable}
-    title="Orders Manager"
+    title="Order Detail Manager"
     columns={columns}
     data={sOrderDetail.order.orderDetails}
     localization={{body: {editRow: {deleteText: 'Are you sure you want to delete this Order?'}}}}
+    editable={{
+        onRowUpdate: (newData) => {
+            return OrderDetail.update(newData).then(([err, data]) => {
+                enqueueSnackbar(err ? err.message : data.message, {variant: err ? "error" : "success"});
+                if(!err){
+                    sOrderDetail.order.orderDetails.map((o) => o.detailId === newData.detailId ? newData : o);
+                }
+            })
+        },
+        onRowDelete: oldData => {
+            return OrderDetail.delete(oldData.detailId).then(([err, data]) => {
+                enqueueSnackbar(err ? err.message : data.message, {variant: err ? "error" : "success"});
+                if(!err){
+                    sOrderDetail.order.orderDetails.filter((o) => o.detailId !== oldData.detailId)
+                }
+            })
+        }
+    }}
     actions={[
         {
             icon: "visibility",
                 tooltip: "Go To Product",
-                onClick: (event, rowData) => {
+                onClick: (rowData) => {
                     rowData = rowData as OrderDetail;
                     window.location.href = `${rowData.link}`
                 }
