5bc8ec47628ba71214d598921ff53383d5bd252b
Pham Dang Khoa <19dh111073@st.huflit.edu.vn>
2022-06-10 13:06:54 +0700

    fix detail display + validation order

--- code/server/src/order/order.service.ts
+++ code/server/src/order/order.service.ts
@@ -97,10 +97,15 @@ export class OrderService {
 
     async findByAccount(id: string){
         const user = await this.userRepository.findOne({where: {userId: id}});
-        if(user.type == UserRoles.USER)
-            return this.orderRepository.find({where: {user: {userId: id}}, relations: {orderDetails: true, partner: true}})
-        else if(user.type == UserRoles.PARTNER)
-            return this.orderRepository.find({where: {partner: {userId: id}}, relations: {orderDetails: true, partner: true}}) 
+        if(user){
+            if(user.type == UserRoles.USER)
+                return this.orderRepository.find({where: {user: {userId: id}}, relations: {orderDetails: true, partner: true}})
+             else if(user.type == UserRoles.PARTNER)
+                return this.orderRepository.find({where: {partner: {userId: id}}, relations: {orderDetails: true, partner: true}})
+        } else {
+            return this.orderRepository.findOne({where: {orderId: id}, relations: {orderDetails: true}})
+        }
+         
     }
 
     findOne(id) {
