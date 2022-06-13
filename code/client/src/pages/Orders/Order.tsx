a2d32ae1d28ea15ae16ee4b89d14d634e64c749a
vinhphan812 <vinhphan812@gmail.com>
2022-06-14 04:10:59 +0700

    sync

--- code/client/src/pages/Orders/Order.tsx
+++ code/client/src/pages/Orders/Order.tsx
@@ -7,10 +7,11 @@ import {useSnackbar} from "notistack";
 import {Order as OrderDTO} from "../../models/Order"
 import {observer} from "mobx-react-lite";
 import {FormControlLabel, Switch} from "@mui/material";
+import {UserRole} from "../../models/types";
 
 export const Order: FC = observer(() => {
     const {enqueueSnackbar} = useSnackbar();
-    const {sOrder} = useStore();
+    const {sOrder, role} = useStore();
 
     useEffect(() => {
         OrderDTO.getOfMe().then(([err, data]) => {
@@ -23,8 +24,8 @@ export const Order: FC = observer(() => {
     }, []);
 
     return <BasicLayout>
-        <FormControlLabel control={<Switch checked={sOrder.mode == "USER"} onChange={() => sOrder.set_mode(sOrder.mode == "USER" ? "PARTNER" : "USER")}/>}
-                          label={sOrder.mode == "USER" ? "USER DISPLAY" : "PARTNER DISPLAY"}/>
+        {role == UserRole.ADMIN && <FormControlLabel control={<Switch checked={sOrder.mode == "USER"} onChange={() => sOrder.set_mode(sOrder.mode == "USER" ? "PARTNER" : "USER")}/>}
+                                                     label={sOrder.mode == "USER" ? "USER DISPLAY" : "PARTNER DISPLAY"}/>}
 
         <OrderResult displayType={sOrder.mode} orders={sOrder.orders}/>
         {/*<OrderTable/>*/}
